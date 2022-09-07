e8ce665d8f5b715ce00bf8562b7aae287d86e301
fdzr <fzamora.reina@gmail.com>
2022-09-07 11:37:22 -0400

    adding library and chinese whispers method

--- src/clustering.py
+++ src/clustering.py
@@ -4,10 +4,12 @@ from collections import Counter
 from sklearn.cluster import SpectralClustering
 import numpy as np
 import scipy
+import networkx as nx
+import chinese_whispers as 
 
-from src.config import UseID, Config
+from src.config import UseID, Config, pairing, sampling
 from src.target import Target
-from src.vector_model import DistanceModel
+from src.vector_model import DistanceModel, VectorModel
 
 
 def split_clusters(clustering: Dict[UseID, int], target: Target) -> Tuple[np.ndarray, np.ndarray]:
@@ -43,5 +45,33 @@ def clustering_spectral(model: DistanceModel, target: Target) -> Dict[UseID, int
     ids = target.uses.identifier.tolist()
     distance_matrix = model.distance_matrix(target)
     labels = clustering.fit_predict(distance_matrix)
-
     return dict(zip(ids, labels))
+
+
+def clustering_chinese_whispers(model: DistanceModel, target: Target) -> Dict[UseID, int]:
+    ids = []
+    G = nx.Graph()
+    # create the edges with their weights
+    compare_ids = sampling.annotated(pairing.COMPARE)
+    earlier_ids = sampling.annotated(pairing.EARLIER)
+    later_ids = sampling.annotated(pairing.LATER)
+    pair_ids = Counter()
+
+    for r, item in target.judgments.iterrows():
+        mean = target.judgments[
+            ((target.judgments.identifier1 == item["identifier1"]) & 
+            (target.judgments.identifier2 == item["identifier2"])) | 
+            ((target.judgments.identifier1 == item["identifier2"]) & 
+            (target.judgments.identifier2 == item["identifier1"]))]["judgment"].mean()
+        pair_ids[(item["identifier1"], item["identifier2"])] = mean
+
+    edges = [(id1, id2, value) for (id1, id2), value in pair_ids.items()]
+    G.add_weighted_edges_from(edges)
+    cw.chinese_whispers(G, weighting="top", seed=1337)
+    new_ids = []
+    new_labels = []
+    for label, values in cw.aggregate_clusters(G).items():
+        new_ids.extend(list(values))
+        new_labels.extend([label for i in range(len(list(values)))])
+    return dict(zip(new_ids, new_labels))
+
