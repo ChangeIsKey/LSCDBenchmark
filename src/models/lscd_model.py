6c25856549a3ca7ea083c58831cda34cc1ff9efc
cbr9 <cabero96@protonmail.com>
2022-09-19 11:24:26 +0200

    created sketch for lscd model

---
+++ src/models/lscd_model.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+from src.target import Target
+
+class LSCDModel(ABC):
+    @abstractmethod
+    def predict(self, targets: list[Target]) -> list[float | int]:
+        ...
+
