eb73f3e7064ba0922aa68e8d45a06973e045900f
Andrés Cabero <cabero96@protonmail.com>
2022-10-05 10:24:14 +0200

    fix: disable graph-tool related code

--- src/wsi/__init__.py
+++ src/wsi/__init__.py
@@ -1,7 +1,13 @@
 from .chinese_whispers import ClusterChineseWhispers
-from .cluster_wsbm import ClusterWSBM
+# from .cluster_wsbm import ClusterWSBM
 from .correlation_clustering import ClusterCorrelation
 from .model import Model
 from .spectral import ClusterSpectral
 
-__all__ = ["Model", "ClusterSpectral", "ClusterChineseWhispers", "ClusterCorrelation", "ClusterWSBM"]
+__all__ = [
+    "Model",
+    "ClusterSpectral",
+    "ClusterChineseWhispers",
+    "ClusterCorrelation",
+    # "ClusterWSBM"
+]
