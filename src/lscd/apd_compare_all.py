6b98b3e32ceb6eebcc4548c6f059a00123b0e6df
Andrés Cabero <cabero96@protonmail.com>
2022-10-12 13:31:29 +0200

    feat: add progress bars to lscd models

--- src/lscd/apd_compare_all.py
+++ src/lscd/apd_compare_all.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tqdm import tqdm
 
 from src import wic
 from src.lscd.model import GradedModel
@@ -8,16 +9,13 @@ from src.target import Target
 class ApdCompareAll(GradedModel):
     wic: wic.Model
 
-    def predict(
-        self,
-        targets: list[Target]
-    ) -> dict[str, float]:
+    def predict(self, targets: list[Target]) -> dict[str, float]:
         predictions = {}
-        for target in targets:
-             use_pairs = target.use_pairs(pairing="COMPARE", sampling="all")
-             with self.wic:
-                 similarities = self.wic.similarities(use_pairs)
-             apd = np.mean(similarities).item()
-             predictions[target.name] = apd
+        for target in tqdm(targets, desc="Generating target predictions"):
+            use_pairs = target.use_pairs(pairing="COMPARE", sampling="all")
+            with self.wic:
+                similarities = self.wic.similarities(use_pairs)
+            apd = np.mean(similarities).item()
+            predictions[target.name] = apd
 
         return predictions
--- src/lscd/cos.py
+++ src/lscd/cos.py
@@ -11,25 +11,22 @@ from src.wic import ContextualEmbedder
 class Cos(GradedModel):
     wic: ContextualEmbedder
 
-    def predict(
-        self,
-        targets: list[Target]
-    ) -> dict[str, float]:
+    def predict(self, targets: list[Target]) -> dict[str, float]:
         predictions = {}
-        for target in tqdm(targets):
-             earlier_df = target.uses_df[target.uses_df.grouping == target.groupings[0]]
-             later_df = target.uses_df[target.uses_df.grouping == target.groupings[1]]
+        for target in tqdm(targets, desc="Generating target predictions"):
+            earlier_df = target.uses_df[target.uses_df.grouping == target.groupings[0]]
+            later_df = target.uses_df[target.uses_df.grouping == target.groupings[1]]
 
-             earlier = [Use.from_series(s) for _, s in earlier_df.iterrows()]
-             later = [Use.from_series(s) for _, s in later_df.iterrows()]
+            earlier = [Use.from_series(s) for _, s in earlier_df.iterrows()]
+            later = [Use.from_series(s) for _, s in later_df.iterrows()]
 
-             with self.wic:
-                 earlier_vectors = np.vstack([self.wic.encode(use) for use in earlier])
-                 later_vectors = np.vstack([self.wic.encode(use) for use in later])
+            with self.wic:
+                earlier_vectors = np.vstack([self.wic.encode(use) for use in earlier])
+                later_vectors = np.vstack([self.wic.encode(use) for use in later])
 
-             earlier_avg = earlier_vectors.mean(axis=0)
-             later_avg = later_vectors.mean(axis=0)
-             cos = distance.cosine(earlier_avg, later_avg)
-             predictions[target.name] = cos
+            earlier_avg = earlier_vectors.mean(axis=0)
+            later_avg = later_vectors.mean(axis=0)
+            cos = distance.cosine(earlier_avg, later_avg)
+            predictions[target.name] = cos
 
         return predictions
