352a17ece918751a312e03d3c4e6ca6961dff36a
Andrés Cabero <cabero96@protonmail.com>
2022-10-12 13:27:32 +0200

    feat: add binary threshold model to exported modules

--- src/lscd/__init__.py
+++ src/lscd/__init__.py
@@ -1,5 +1,6 @@
 from .apd_compare_all import ApdCompareAll
 from .cluster_jsd import ClusterJSD
 from .cos import Cos
+from .model import BinaryThresholdModel
 
-__all__ = ["ApdCompareAll", "Cos", "ClusterJSD", ]
+__all__ = ["ApdCompareAll", "Cos", "ClusterJSD", "BinaryThresholdModel"]
