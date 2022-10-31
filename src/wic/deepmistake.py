17e7411489b449e855295b9720789a9156348c93
Andrés Cabero <cabero96@protonmail.com>
2022-10-31 15:27:06 +0100

    run deepmistake on one target at a time

--- src/wic/deepmistake.py
+++ src/wic/deepmistake.py
@@ -14,27 +14,24 @@ class DeepMistake(WICModel):
     def predict(
         self, use_pairs: list[tuple[Use, Use]]
     ) -> list[float]:
-        target2usepairs = defaultdict(list)
-        for up in use_pairs:
-            target2usepairs[up[0].target].append(to_data_format(up))
-        target2path = {}
-        with tempfile.TemporaryDirectory() as tmp_dir:
-            path = Path(tmp_dir)
-            for target in target2usepairs.keys():
-                data = path / f"{target}.data"
-                with open(data, mode="w", encoding="utf8") as f:
-                    json.dump(target2usepairs[target], f)
-                target2path[target] = data
+        data = [to_data_format(up) for up in use_pairs]
+        dir = Path("deepmistake")
+        output = dir / "scores"
+        path = dir / f"{use_pairs[0][0].target}.data"
 
-            scores_path = path / "scores"
-            os.system(f"python -u run_model.py \
-                --max_seq_len=500 \
-                --do_eval --ckpt_path {self.checkpoint.parent} \
-                --eval_input_dir {path} \
-                --eval_output_dir {scores_path} \
-                --output_dir {self.checkpoint.parent}") 
+        with open(path, mode="w", encoding="utf8") as f:
+            json.dump(data, f)
+
+        scores_path = dir / "scores"
+        os.system(f"python -u run_model.py \
+            --max_seq_len=500 \
+            --do_eval \
+            --ckpt_path {self.checkpoint.parent} \
+            --eval_input_dir {dir} \
+            --eval_output_dir {output} \
+            --output_dir {output}") 
             
-             
+        return [] 
 
 
 # cd mcl-wic
