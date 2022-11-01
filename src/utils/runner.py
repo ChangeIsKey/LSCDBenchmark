ecdb2905c2dbed3c067ee1601da0737b4b83d2bd
Andrés Cabero <cabero96@protonmail.com>
2022-11-01 11:54:03 +0100

    feat: add progress bar to runner.py

--- src/utils/runner.py
+++ src/utils/runner.py
@@ -4,6 +4,7 @@ from hydra import utils
 from numpy import isin
 import numpy as np
 from omegaconf import DictConfig
+from tqdm import tqdm
 from src.dataset import Dataset
 from src.evaluation import Evaluation
 from src.wic.model import ThresholdedWicModel, WICModel
@@ -27,10 +28,11 @@ def run(
     dataset: Dataset, model: Model, evaluation: Evaluation, write: bool = True
 ) -> float:
     predictions = {}
+    lemma_pbar = tqdm(dataset.lemmas, desc="Processing lemmas")
     if isinstance(model, (ThresholdedWicModel, WICModel)):
         assert dataset.sampling is not None
         assert dataset.pairing is not None
-        for lemma in dataset.lemmas:
+        for lemma in lemma_pbar:
             use_pairs = []
             for s, p in list(zip(dataset.sampling, dataset.pairing)):
                 use_pairs += lemma.use_pairs(pairing=p, sampling=s)
@@ -40,16 +42,16 @@ def run(
             predictions.update(dict(zip(id_pairs, model.predict(use_pairs))))
             # TODO: call thresholding for WIC models
     elif isinstance(model, GradedModel):
-        for lemma in dataset.lemmas:
+        for lemma in lemma_pbar:
             predictions.update({lemma.name: model.predict(lemma)})
     elif isinstance(model, BinaryThresholdModel):
         graded_predictions = []
         lemma_names = [lemma.name for lemma in dataset.lemmas]
-        for lemma in dataset.lemmas:
+        for lemma in lemma_pbar:
             graded_predictions.append(model.graded_model.predict(lemma))
         predictions = dict(zip(lemma_names, model.predict(graded_predictions)))
     elif isinstance(model, WSIModel):
-        for lemma in dataset.lemmas:
+        for lemma in lemma_pbar:
             uses = lemma.get_uses()
             ids = [use.identifier for use in uses]
             predictions.update(dict(zip(ids, model.predict(uses))))
