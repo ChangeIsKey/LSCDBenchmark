02d8151dcaacb53d2456bf0e1ae4894b5ab8dd0e
Andrés Cabero <cabero96@protonmail.com>
2022-11-11 22:37:33 +0100

    fix type

--- src/wsi/spectral.py
+++ src/wsi/spectral.py
@@ -9,14 +9,11 @@ from src.wsi.model import WSIModel
 class ClusterSpectral(WSIModel):
     n_clusters: int
 
-    class Config:
-        arbitrary_types_allowed = True
-
     def predict(self, uses: list[Use]) -> list[int]:
         clustering = SpectralClustering(
             n_clusters=self.n_clusters, assign_labels="kmeans", affinity="precomputed"
         )
-        use_pairs = set(combinations(uses, r=2))
+        use_pairs = list(combinations(uses, r=2))
         similarity_matrix = self.similarity_matrix(use_pairs)
         labels = clustering.fit_predict(similarity_matrix)
         return labels
