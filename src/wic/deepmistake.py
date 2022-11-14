957c8120e1805c5eef651e4854be4a3eaa917e02
Andrés Cabero <cabero96@protonmail.com>
2022-11-14 10:35:47 +0100

    feat: automatically download deepmistake checkpoints

--- src/wic/deepmistake.py
+++ src/wic/deepmistake.py
@@ -1,29 +1,83 @@
 import json
 import os
-from typing import TypedDict
-import numpy as np
 from pathlib import Path
+import shutil
+from typing import Literal, TypedDict
+
+import zipfile
+import numpy as np
+import requests
+from pydantic import BaseModel, HttpUrl
+from tqdm import tqdm
 
 from src.use import Use, to_data_format
-from src.wic.model import WICModel
 from src.utils import utils
+from src.wic.model import WICModel
 
-
-class Model(TypedDict):
-    path: Path
-    
-
+class Model(BaseModel):
+    name: str
+    url: HttpUrl
 
 class DeepMistake(WICModel):
-    model: Model
+    ckpt: Model
 
-    def __init__(self, **data) -> None:
-        super().__init__(**data)
-        self.model["path"] = utils.path(str(self.model["path"]))
+    @property
+    def cache_dir(self) -> Path:
+        cache = os.getenv("DEEPMISTAKE_CKPTS")
+        if cache is None:
+            cache = ".deepmistake"
+        return utils.path(cache)
+        
+    @property
+    def ckpt_dir(self) -> Path:
+        return self.cache_dir / self.ckpt.name
+
+    def __unzip_ckpt(self, zipped: Path) -> None:
+        with zipfile.ZipFile(file=zipped) as z:
+            namelist = z.namelist()[1:]  # remove root element
+
+            for filename in tqdm(namelist, desc="Unzipping checkpoint files", leave=False):
+                filename_p = Path(filename)
+                path = self.ckpt_dir / filename_p.parts[-1]
+                with path.open(mode="wb") as file_obj:
+                    shutil.copyfileobj(z.open(filename, mode="r"), file_obj)
+
+        zipped.unlink()
+
+
+    def __download_ckpt(self) -> Path:
+        filename = self.ckpt.url.split('/')[-1]
+        assert filename.endswith(".zip")
+
+        r = requests.get(self.ckpt.url, stream=True)
+        ckpt_dir = self.ckpt_dir
+        ckpt_dir.mkdir(parents=True, exist_ok=True)
+        path = ckpt_dir / filename
+
+        with open(file=path, mode="wb") as f:
+            pbar = tqdm(
+                desc=f"Downloading checkpoint '{self.ckpt.name}'",
+                unit="B",
+                unit_scale=True,
+                unit_divisor=1024,
+                total=int(r.headers["Content-Length"]),
+                leave=False,
+            )
+            pbar.clear()  # clear 0% info
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:  # filter out keep-alive new chunks
+                    pbar.update(len(chunk))
+                    f.write(chunk)
+            pbar.close()
+        return path
 
     def predict(self, use_pairs: list[tuple[Use, Use]]) -> list[float]:
-        data_dir = self.model["path"] / "data"
-        output_dir = self.model["path"] / "scores"
+        if not self.ckpt_dir.exists():
+            zipped = self.__download_ckpt()
+            self.__unzip_ckpt(zipped)
+
+        data_dir = self.ckpt_dir / "data"
+        output_dir = self.ckpt_dir / "scores"
         output_dir.mkdir(parents=True, exist_ok=True)
         data_dir.mkdir(parents=True, exist_ok=True)
 
@@ -36,12 +90,12 @@ class DeepMistake(WICModel):
 
         hydra_dir = os.getcwd()
 
-        os.chdir(self.model["path"])
+        os.chdir(self.ckpt_dir)
         os.system(
             f"python -u {script} \
             --max_seq_len=500 \
             --do_eval \
-            --ckpt_path {self.model['path']} \
+            --ckpt_path {self.ckpt_dir} \
             --eval_input_dir {data_dir} \
             --eval_output_dir {output_dir} \
             --output_dir {output_dir}"
