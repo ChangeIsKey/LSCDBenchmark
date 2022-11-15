338c0eb05f25bf7b84e18e7ec08df2b0626e8f71
Andrés Cabero <cabero96@protonmail.com>
2022-11-15 23:33:58 +0100

    fix command

--- src/wic/deepmistake.py
+++ src/wic/deepmistake.py
@@ -246,7 +246,7 @@ class DeepMistake(WICModel):
                 os.chdir(self.ckpt_dir)
                 output = subprocess.run([
                     "python",
-                    "-u {script}",
+                    f"-u {script}",
                     "--max_seq_len=500",
                     "--do-eval",
                     f"--ckpt_path {self.ckpt_dir}",
