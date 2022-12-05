ada8405da2c07d4fe1243b326b842868313f3ff5
Andrés Cabero <cabero96@protonmail.com>
2022-12-05 01:50:05 +0100

    more docstrings

--- src/lemma.py
+++ src/lemma.py
@@ -72,6 +72,14 @@ class Lemma(BaseModel):
 
     @property
     def uses_df(self) -> DataFrame:
+        """Cached property that collects the corresponding uses.csv files,
+        as well as preprocesses each use based on the provided configuration.
+        
+        Returns
+        -------
+        DataFrame
+            The preprocessed DataFrame of uses for the corresponding lemma
+        """
         if self._uses_df is None:
             # load uses
             path = self.path / "uses.csv"
@@ -104,14 +112,31 @@ class Lemma(BaseModel):
 
     @property
     def annotated_pairs_df(self) -> DataFrame:
+        """Property that collects the annotated pairs of the corresponding lemma
+        from its judgments.csv file. It performs validation based on :attr:`annotated_pairs_schema`.
+
+        Returns
+        -------
+        DataFrame
+            A DataFrame containing two columns (identifier1, identifier2)
+        """
         if self._annotated_pairs_df is None:
             path = self.path / "judgments.csv"
-            self._annotated_pairs_df = pd.read_csv(path, delimiter="\t", encoding="utf8", quoting=csv.QUOTE_NONE, usecols=["identifier1", "identifier2"])
-            self.annotated_pairs_schema.validate(self._annotated_pairs_df)
+            self._annotated_pairs_df = pd.read_csv(path, delimiter="\t", encoding="utf8", quoting=csv.QUOTE_NONE)
+            self._annotated_pairs_df = self.annotated_pairs_schema.validate(self._annotated_pairs_df)
+            self._annotated_pairs_df = self._annotated_pairs_df.loc[["identifier1", "identifier2"]]
         return self._annotated_pairs_df
 
     @property
     def augmented_annotated_pairs_df(self) -> DataFrame:
+        """A version of :attr:`annotated_pairs_df` that incorporates grouping information.
+        The base :attr:`annotated_pairs_df` is expanded with the groupings oƒ each of the identifiers in each row. 
+
+        Returns
+        -------
+        DataFrame
+            The expanded DataFrame
+        """
         if self._augmented_annotated_pairs is None:
             self._augmented_annotated_pairs = pd.merge(
                 self.annotated_pairs_df,
@@ -135,6 +160,13 @@ class Lemma(BaseModel):
 
     @property
     def annotated_pairs_schema(self) -> DataFrameSchema:
+        """Schema for validating that a judgments.csv file contains two columns (identifier1, identifier2)
+
+        Returns
+        -------
+        DataFrameSchema
+            The schema
+        """
         return DataFrameSchema(
             {
                 "identifier1": Column(dtype=str),
@@ -143,9 +175,24 @@ class Lemma(BaseModel):
         )
 
     def useid_to_grouping(self) -> Dict[UseID, str]:
+        """Method to generate a dictionary from use identifiers to their corresponding groupings
+
+        Returns
+        -------
+        Dict[UseID, str]
+            A dictionary from use identifiers to use groupings
+        """
         return dict(zip(self.uses_df.identifier, self.uses_df.grouping))
 
     def grouping_to_useid(self) -> dict[str, list[UseID]]:
+        """Method to generate a dictionary from use groupings to a 
+        list of use identifiers corresponding to that grouping
+
+        Returns
+        -------
+        Dict[UseID, str]
+            A dictionary from groupings to list of use identifier
+        """
         grouping_to_useid = defaultdict(list)
         for useid, grouping in self.useid_to_grouping().items():
             grouping_to_useid[grouping].append(useid)
@@ -165,6 +212,18 @@ class Lemma(BaseModel):
         return ids.identifier.tolist(), ids.identifier.tolist()
 
     def split_uses(self, pairing: Pairing) -> tuple[list[UseID], list[UseID]]:
+        """Splits the uses of a lemma into two separate lists of use identifiers, according to `pairing`
+
+        Parameters
+        ----------
+        pairing : Pairing
+            A pairing strategy
+
+        Returns
+        -------
+        tuple[list[UseID], list[UseID]]
+            _description_
+        """
         match pairing:
             case "COMPARE":
                 return self._split_compare_uses()
