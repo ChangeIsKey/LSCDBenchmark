e0f7690c4c5ebb040042955ac154e9645b1cae91
Andrés Cabero <cabero96@protonmail.com>
2022-12-11 23:04:34 +0100

    implement diasense model

--- src/lscd/apd.py
+++ src/lscd/apd.py
@@ -27,6 +27,26 @@ class APD(GradedLSCDModel):
         return [self.predict(lemma) for lemma in tqdm(lemmas, desc="Processing lemmas")]
 
 
+class DiaSense(GradedLSCDModel):
+    wic: wic.WICModel
+
+    def predict(self, lemma: Lemma) -> float:
+        use_pairs_0 = lemma.use_pairs(pairing="COMPARE", sampling="all")
+        use_pairs_1 = lemma.use_pairs(pairing="ALL", sampling="all")
+        similarities_0 = self.wic.predict(use_pairs_0)
+        similarities_1 = self.wic.predict(use_pairs_1)
+        return np.mean(similarities_0).item() - np.mean(similarities_1).item()
+
+    def predict_all(self, lemmas: list[Lemma]) -> list[float]:
+        use_pairs_nested = [
+            lemma.use_pairs(pairing="ALL", sampling="all") 
+            for lemma in tqdm(lemmas, desc="Building use pairs", leave=False)
+        ]
+        use_pairs = [use_pair for sublist in use_pairs_nested for use_pair in sublist]
+        id_pairs = [(use_0.identifier, use_1.identifier) for use_0, use_1 in use_pairs]
+        self.wic.predictions = dict(zip(id_pairs, self.wic.predict_all(use_pairs=use_pairs)))
+        return [self.predict(lemma) for lemma in tqdm(lemmas, desc="Processing lemmas")]
+
 ApdCompareAll = partial(APD, pairing="COMPARE", sampling="all")
 ApdEarlierAll = partial(APD, pairing="EARLIER", sampling="all")
 ApdLaterAll = partial(APD, pairing="LATER", sampling="all")
