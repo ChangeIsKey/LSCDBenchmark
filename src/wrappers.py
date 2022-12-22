9025d3211cbabe9a388526bd08832e7ed3daa243
Andrés Cabero <cabero96@protonmail.com>
2022-12-22 19:10:36 +0100

    make evaluation metric a config parameter

--- src/wrappers.py
+++ src/wrappers.py
@@ -1,7 +1,12 @@
 from typing import Literal
+
 from scipy import stats
 
 
 def spearmanr(y_true: list[float], y_pred: list[float], **kwargs) -> float:
     corr, _ = stats.spearmanr(a=y_true, b=y_pred, **kwargs)
+    return corr
+
+def pearsonr(y_true: list[float], y_pred: list[float], **kwargs) -> float:
+    corr, _ = stats.pearsonr(x=y_true, y=y_pred, **kwargs)
     return corr
