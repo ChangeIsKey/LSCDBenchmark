2e83f918c9b984b05875fd603ddf7bae118de39c
kuan-yu-lin <kuanyulin.lky@gmail.com>
2023-04-19 15:10:02 +0200

    modify theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,27 +1,90 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# For the full list of built-in configuration values, see the documentation:
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# -- Path setup --------------------------------------------------------------
 
-project = 'LSCDBenchmark'
-copyright = '2023, Change is the key!'
-author = 'Change is the key!'
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'LSCD Benchmark'
+copyright = '2023, CIK'
+author = 'CIK'
+
+# The full version, including alpha/beta/rc tags
+release = '0.1'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.viewcode",
+    "sphinx.ext.todo",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    # "sphinxcontrib.autodoc_pydantic",
+    "autoapi.extension"
+]
 
+autoapi_dirs = ["../../src"]
+# autosummary_generate = True
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    "matplotlib": ('https://matplotlib.org/stable/', None),
+    "numpy": ('https://numpy.org/doc/stable/', None),
+    "pandas": ('https://pandas.pydata.org/docs/', None),
+    "torch": ("https://pytorch.org/docs/stable/", None),
+}
+
+autodoc_typehints_format = "fully-qualified"
+
+
+autodoc_default_options = {
+    'member-order': 'groupwise',
+    'members': True,
+    'undoc-members': True,
+    'show-inheritance': True,
+    'inherited-members': "BaseModel"
+}
+autodoc_typehints = 'signature'
+autoclass_content = 'both'
+
+
+# autoapi_dirs = ["../../src"]
+
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_book_theme'
+
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
