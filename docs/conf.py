9d6eea86954e265ffbddeed26bdb2ab5162d85bd
kuan-yu-lin <kuanyulin.lky@gmail.com>
2023-04-21 10:48:33 +0200

    update docs to test the new theme

--- docs/conf.py
+++ docs/conf.py
@@ -13,17 +13,18 @@ author = 'Change is the key!'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-import sphinx_rtd_theme
-
-extensions = ['sphinx_rtd_theme', "myst_parser"]
+extensions = ["myst_parser"]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+import sphinx_book_theme
+
+html_theme = 'sphinx_book_theme'
+html_theme_path = [sphinx_book_theme.get_html_theme_path()]
 html_static_path = ['_static']
