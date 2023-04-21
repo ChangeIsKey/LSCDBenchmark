fe9298235371e981c00917a0546b8474862fd07f
kuan-yu-lin <kuanyulin.lky@gmail.com>
2023-04-21 12:01:29 +0200

    add github link

--- docs/conf.py
+++ docs/conf.py
@@ -28,3 +28,18 @@ import sphinx_book_theme
 html_theme = 'sphinx_book_theme'
 html_theme_path = [sphinx_book_theme.get_html_theme_path()]
 html_static_path = ['_static']
+
+html_theme_options = {
+    "icon_links": [
+        {
+            # Label for this link
+            "name": "GitHub",
+            # URL where the link will redirect
+            "url": "https://github.com/ChangeIsKey/LSCDBenchmark",  # required
+            # Icon class (if "type": "fontawesome"), or path to local image (if "type": "local")
+            "icon": "fa-brands fa-square-github",
+            # The type of image to be used (see below for details)
+            "type": "fontawesome",
+        }
+   ]
+}
