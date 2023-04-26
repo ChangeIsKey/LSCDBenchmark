6bb8ecfd379aa12d37dd482e8531825d695d04f7
kuan-yu-lin <kuanyulin.lky@gmail.com>
2023-04-26 15:16:29 +0200

    add abspath back

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,10 @@ import sphinx_book_theme
 
 import autoapi
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
