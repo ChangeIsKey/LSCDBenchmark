95577602222d37c6cdcea42c0c3c8910db986880
kuan-yu-lin <kuanyulin.lky@gmail.com>
2023-04-26 14:32:55 +0200

    change path to ../LSCDBenchmark/src

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@ import sphinx_book_theme
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src'))
+sys.path.insert(0, os.path.abspath('../LSCDBenchmark/src'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
