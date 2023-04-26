fb44be23c9e8c3cb9ca2ed9bfb34dc1732777e47
kuan-yu-lin <kuanyulin.lky@gmail.com>
2023-04-26 16:05:22 +0200

    test autoapi_dirs

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@ import autoapi
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -28,7 +28,7 @@ extensions = [
             "autoapi.extension"
               ]
 
-autoapi_dirs = ['../../src']
+autoapi_dirs = ['../src']
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
